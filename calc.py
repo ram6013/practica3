3786fc95ad1342e1850dcab92f8aa172d1bf2417
Ramón <134057913+ram6013@users.noreply.github.com>
2025-10-06 19:41:31 +0200

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,10 @@
+def add(a, b):
+  return a + b
+def subtract(a, b):
+  return a - b
+
+  # test_calculator.py
+def test_addition():
+  assert add(1, 2) == 3
+def test_subtraction():
+  assert subtract(2, 1) == 1
